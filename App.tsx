ab69e809ce4400bac8301620b903496aa85da1c7
Valmir Almeida <almeidavalmir76@gmail.com>
2022-09-14 22:51:07 -0300

    :package: first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
+import { Background } from "./src/components/background";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      ></StatusBar>
+    </Background>
+  );
+}
